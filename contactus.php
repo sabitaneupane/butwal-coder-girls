294638853abf2bc321d79f4f00e9f47e0b32ed44
Sabita Neupane <nsabita.sasa@gmail.com>
2020-01-03 17:05:14 +0545

    Changing php files into html

--- contactus.php
+++
@@ -1,7 +0,0 @@
-<?php include('header.php');?>
-	<section>
-		<div id="section"  style="height:1000px;">
-			<h1>Contact us</h1>
-		</div>
-	</section>			
-<?php include('footer.php');?>
--- interview.php
+++
@@ -1,7 +0,0 @@
-<?php include('header.php');?>
-	<section>
-		<div id="section"  style="height:1000px;">
-			<h1>Interview</h1>
-		</div>
-	</section>			
-<?php include('footer.php');?>
